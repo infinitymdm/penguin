5b65e0e006fd883330bf3ad1e463f98646713405
Marcus Mellor <infinitymdm@gmail.com>
2025-03-07 15:37:59 -0600

    keccak: Add workaround for yosys' failure to support streaming operators

--- src/keccak/keccak_iota.sv
+++ src/keccak/keccak_iota.sv
@@ -10,7 +10,13 @@ module keccak_iota #(
         for (genvar i = 0; i < 5; i++) begin: sheet_select
             for (genvar j = 0; j < 5; j++) begin: lane_select
                 if ((i == 4) & (j == 4)) begin: apply_rc
-                    assign y[i][j] = x[i][j] ^ {<<8{rc}};
+                    // If yosys supported streaming operators, we'd flip the rc bytes like so:
+                    // assign y[i][j] = x[i][j] ^ {<<8{rc}};
+                    // Unfortunately yosys doesn't support the {<<{}} streaming operator :(
+                    // Instead we have to swap bytes manually, adding this gross for loop
+                    for (genvar b = 0; b < w/8; b++) begin: swap_bytes
+                        assign y[i][j][8*(b+1)-1:8*b] = x[i][j][8*(b+1)-1:8*b] ^ rc[w-8*b-1:w-8*(b+1)];
+                    end
                 end else begin: passthrough
                     assign y[i][j] = x[i][j];
                 end
