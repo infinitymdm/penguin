bc10fade9c40faa08ddd289744bc1323d32d3c31
Marcus Mellor <infinitymdm@gmail.com>
2025-03-31 13:55:30 -0500

    tb: working testbench with NIST short messages

--- tb/tb_sha3_nist.sv
+++ tb/tb_sha3_nist.sv
@@ -24,9 +24,9 @@ module tb_sha3_nist;
     int waited_cycles;
     int len, chunk_index;
     bit eof, no_more_chunks;
-    string message_full, expected_digest, temp;
+    string message_full, temp;
     bit [r-1:0] message_chunk;
-    bit [d-1:0] digest;
+    bit [d-1:0] expected_digest, digest;
 
     bit clk, reset, enable;
     keccak #(d, 6, s) dut (
@@ -48,7 +48,7 @@ module tb_sha3_nist;
     always #5 clk <= ~clk;
 
     initial begin: open_rsp_file
-        rsp_file = $fopen(rsp_file_name, "rb");
+        rsp_file = $fopen(rsp_file_name, "r");
         if (rsp_file == 0) $error("Unable to open file '%s'", rsp_file_name);
     end
 
@@ -57,9 +57,10 @@ module tb_sha3_nist;
         output bit eof,
         output int len,
         output string message_full,
-        output string expected_digest
+        output bit [d-1:0] expected_digest
     );
         string line, value;
+        byte digest_byte;
 
         // scan Len
         while (!$feof(rsp_file)) begin: scan_Len
@@ -78,7 +79,10 @@ module tb_sha3_nist;
             $fgets(line, rsp_file);
             if ($sscanf(line, "MD = %s\n", value) == 1) break;
         end
-        expected_digest = value;
+        for (int i = 0; 8*i < d; i++) begin: read_expected_digest_bytes
+            digest_byte = value.substr(2*i, 2*i+1).atohex();
+            expected_digest = {expected_digest[d-9:0], digest_byte};
+        end
         eof = $feof(rsp_file);
     endtask
 
@@ -89,16 +93,33 @@ module tb_sha3_nist;
         output bit [r-1:0] message_chunk,
         output bit no_more_chunks
     );
-        int chunk_len = len - (r * index);
+        int remaining_len = len - (r * index);
+        byte message_byte;
         bit [1:0] suffix = 2'b01;
 
-        if (chunk_len < r) begin: pad_chunk
+        if (remaining_len < r) begin: pad_chunk
             no_more_chunks = 1;
-            // TODO: Iterate over the string one byte at a time. This won't work because atohex only returns integers
-            message_chunk = {>>{message_full.atohex()[r*index+:r], suffix}} & 'b01;
+            // convert one byte at a time until out of bytes
+            for (int i = 0; 8*i < remaining_len; i++) begin: read_partial_slice_bytes
+                message_byte = message_full.substr(2*i, 2*i+1).atohex();
+                message_chunk = {message_chunk[r-9:0], message_byte};
+            end
+            // pad according to SHA-3 pad10*1 rule
+            if (r - remaining_len == 8)
+                message_chunk = {message_chunk[r-9:0], 8'h86}; // Special case where only one byte remains
+            else begin: pad10_1
+                message_chunk = {message_chunk[r-9:0], 8'h06};
+                for (int i = 0; 8*i < r - remaining_len - 16; i++)
+                    message_chunk = {message_chunk[r-9:0], 8'h00};
+                message_chunk = {message_chunk[r-9:0], 8'h80};
+            end
         end else begin: slice_chunk
             no_more_chunks = 0;
-            message_chunk = message_full.atohex()[r*index+:r];
+            // convert one byte at a time until slice is full
+            for (int i = 0; 8*i < r; i++) begin: read_full_slice_bytes
+                message_byte = message_full.substr(2*i, 2*i+1).atohex();
+                message_chunk = {message_chunk[r-9:0], message_byte};
+            end
         end
     endtask
 
@@ -108,28 +129,46 @@ module tb_sha3_nist;
         chunk_index = 0;
     end
 
-    string pls_delete_me;
-
     always @(posedge clk) begin: stimulate_dut
-        if (!eof) begin: get_next_message_or_wait_for_next_chunk
+        if (waited_cycles % s == 0)
             if (no_more_chunks) begin: get_next_test_vector
-                read_test_vector(rsp_file, eof, len, message_full, expected_digest);
-                $display("Msg = %s", message_full);
-                $display("MD = %s", expected_digest);
-                chunk_index = 0;
-            end else begin: wait_for_next_chunk
-                if (waited_cycles % s == 0) begin: get_next_chunk
-                    $display("digest = %h", digest);
-                    get_message_chunk(len, chunk_index, message_full, message_chunk, no_more_chunks);
-                    chunk_index++;
+                // Check results if the dut was previously enabled
+                if (enable) begin: check_result
+                    $display("nist:   %h", expected_digest);
+                    $display("digest: %h", digest);
+                    if (digest == expected_digest) begin: result_pass
+                        $write("%c[1;32m", 27); // Set style bold, green foreground
+                        $display("PASS");
+                        $write("%c[0m", 27); // reset graphics modes
+                    end else begin: result_fail
+                        $write("%c[1;30m", 27); // Set style bold, red foreground
+                        $display("FAIL");
+                        $error("Failed on message %s with %d bits!", message_full, len);
+                    end
                 end
-                waited_cycles++;
+
+                // Reset and load the next test vector
+                enable = 0;
+                reset = 1;
+                #1;
+                reset = 0;
+                read_test_vector(rsp_file, eof, len, message_full, expected_digest);
+                no_more_chunks = 0; // Force next cycle to fetch a new chunk
+                chunk_index = 0;
+
+                // If we reached eof while looking for the next test vector, exit
+                if (eof) begin: handle_eof
+                    $display("Reached EOF. Exiting...");
+                    $fclose(rsp_file);
+                    $finish;
+                end
+            end else begin: get_next_chunk
+                // Load the next chunk of the message (with padding applied as appropriate)
+                get_message_chunk(len, chunk_index, message_full, message_chunk, no_more_chunks);
+                chunk_index++;
+                enable = 1;
             end
-        end else begin: handle_eof
-            enable = 0;
-            $fclose(rsp_file);
-            $finish;
-        end
+        waited_cycles++;
     end
 
 endmodule
