721760604bc6ab8743b2a5ff977a0337b8d201c2
Marcus Mellor <infinitymdm@gmail.com>
2026-06-03 16:15:24 -0500

    sc: update top, improve build

--- sc/make.py
+++ sc/make.py
@@ -1,7 +1,34 @@
 #!/usr/bin/env python3
 
-from siliconcompiler import Design, ASIC, Lint
+"""SHA3 Performance Experiments
+
+Experiment Design
+
+Goal: The purpose of this experiment is to characterize the power, performance, and area of a SHA3
+hash function hardware design with varying parameters. There are two designs under consideration:
+
+- A pipelined design which splits the keccak round into steps: theta/rho/pi, then chi/iota.
+- A speed-optimized design which may perform multiple keccak rounds per clock cycle.
+
+The design should be tested under all possible configurations and multiple PDKs.
+
+The experiment is as follows:
+1. Test and synthesize each design configuration.
+    - Digest lengths: 512, 384, 256, 224
+    - Cycles per chunk: 48 (2-cycle pipelined), 24, 12, 8, 6, 4, 3, 2 1
+2. Measure power, performance, and area results for each configuration.
+    - Dynamic and leakage power
+    - Critical path, fmax, throughput
+    - Core area
+3. Analyze results
+
+"""
+
+from pathlib import Path
+
+from siliconcompiler import Design, ASIC, Lint, Sim
 from siliconcompiler.flows.lintflow import LintFlow
+from siliconcompiler.flows.dvflow import DVFlow
 from siliconcompiler.targets import asic_target
 
 class PenguinSHA3Design(Design):
@@ -10,7 +37,7 @@ class PenguinSHA3Design(Design):
         self.set_name('penguin_sha3')
         self.set_dataroot('penguin',
                           'git+https://github.com/infinitymdm/penguin.git',
-                          'c9ee678bd5b7b09b591302acf83142c58f425852')
+                          '1002ee655a49f809df806c865a18be604b6ad327')
         self.set_dataroot('local', __file__)
 
         with self.active_fileset('rtl'):
@@ -31,6 +58,32 @@ class PenguinSHA3Design(Design):
             with self.active_dataroot('local'):
                 self.add_file('penguin_sha3.sdc')
 
+        with self.active_fileset('testbench'):
+            with self.active_dataroot('penguin'):
+                self.add_file('tb/tb_sha3_pipelined.sv')
+
+
+def check():
+    assert PenguinSHA3Design().check_filepaths()
+
+
+def sim(tool: str = 'verilator', **rtl_params):
+    project = Sim()
+    design = PenguinSHA3Design()
+    project.set_design(design)
+    project.add_fileset('rtl')
+    project.add_fileset('testbench')
+
+    # Override parameters if given
+    for param, value in rtl_params.items():
+        design.set_param(param, str(value), fileset='testbench')
+
+    project.set_flow(DVFlow(tool=tool))
+
+    project.run()
+    project.summary()
+
+
 def lint(fileset: str = 'rtl'):
     project = Lint()
     project.set_design(PenguinSHA3Design())
@@ -40,11 +93,18 @@ def lint(fileset: str = 'rtl'):
     project.run()
     project.summary()
 
-def syn(fileset: str = 'rtl', pdk: str = 'gf180'):
+
+def syn(pdk: str = 'gf180', **rtl_params):
     project = ASIC()
-    project.set_design(PenguinSHA3Design())
-    project.add_fileset(fileset)
+    design = PenguinSHA3Design()
+    project.set_design(design)
+    project.add_fileset('rtl')
     project.add_fileset(f'sdc.{pdk}')
+
+    # Override parameters if given
+    for param, value in rtl_params.items():
+        design.set_param(param, str(value), fileset='rtl')
+
     asic_target(project, pdk=pdk)
 
     # Design constraints
@@ -54,14 +114,13 @@ def syn(fileset: str = 'rtl', pdk: str = 'gf180'):
     # Use slang for synthesis, since we're using systemverilog sources
     project.set('tool', 'yosys', 'task', 'syn_asic', 'var', 'use_slang', True)
 
-    # [print(k) for k in project.allkeys()]
-
     project.run()
     project.summary()
     project.snapshot()
 
-def check():
-    assert PenguinSHA3Design().check_filepaths()
 
 if __name__ == "__main__":
-    syn()
+    check()
+    # lint()
+    # syn()
+    sim(M=Path('../README').absolute())
--- sc/penguin_sha3_top.sv
+++ sc/penguin_sha3_top.sv
@@ -1,11 +1,13 @@
 `timescale 1 ns / 1 ps
 
-module penguin_sha3_top (
-    input  logic         clk, reset, enable,
-    input  logic   [1:0] op,
-    input  logic   [4:0] round,
-    input  logic [575:0] message,
-    output logic [511:0] digest
+module penguin_sha3_top #(
+    parameter D = 512
+) (
+    input  logic              clk, reset, enable,
+    input  logic              op,
+    input  logic        [4:0] round,
+    input  logic [1599-2*D:0] message,
+    output logic      [D-1:0] digest
 );
-    keccak_pipelined #(.D(512)) sha3_512_pipelined (.*);
+    keccak_pipelined #(.D(D)) sha3_pipelined (.*);
 endmodule
